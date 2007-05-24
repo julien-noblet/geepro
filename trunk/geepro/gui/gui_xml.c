e43fb18684a4a8be8e9f5dba85d2aece92be4527
krzkomar <krzkomar@2b195e0a-49af-4348-9a60-1f55b68a0e1c>
2007-05-24 11:51:15 +0000

    xml gui builder added widgets, willem test 'gui' corrected

--- trunk/geepro/gui/gui_xml.c
+++ trunk/geepro/gui/gui_xml.c
@@ -1,4 +1,4 @@
-/* $Revision: 1.1 $ */
+/* $Revision: 1.2 $ */
 /* hex, binary viewer, editor, kontrolka GTK
  * Copyright (C) 2007 Krzysztof Komarnicki
  * Email: krzkomar@wp.pl
@@ -80,15 +80,15 @@ static void gui_xml_container_add(gui *g, xmlNode *cur, xmlDocPtr doc, GtkWidget
 
     if(pos){
 	sscanf(pos, "%i, %i, %i, %i", &b0, &b1, &b2, &b3);
-	gtk_table_attach(GTK_TABLE(parent), child, b0, b1, b2, b3, flagx, flagy, 0,0);
+	gtk_table_attach(GTK_TABLE(parent), child, b0, b1, b2, b3, flagx, flagy, spx, spy);
     }else
 	gtk_container_add(GTK_CONTAINER(parent), child);
 }
 
-GtkWidget *gui_xml_frame_new(xmlNode *cur)
+static GtkWidget *gui_xml_frame_new(xmlNode *cur)
 {
     char *label, *align, *shadow;
-    float a0=0, a1=0;
+    int a0=0, a1=0;
     int bn;
     GtkWidget *tmp;
 
@@ -97,8 +97,8 @@ GtkWidget *gui_xml_frame_new(xmlNode *cur)
     shadow = (char *)xmlGetProp(cur, (unsigned char *)"shadow");
     tmp = gtk_frame_new(label);
     if(align){
-	sscanf(align, "%f, %f", &a0, &a1);
-	gtk_frame_set_label_align(GTK_FRAME(tmp), a0, a1);
+	sscanf(align, "%i, %i", &a0, &a1);
+	gtk_frame_set_label_align(GTK_FRAME(tmp), (float)a0 / 100, (float)a1 / 100);
     }
     if(shadow){
 	bn = GTK_SHADOW_NONE;
@@ -130,29 +130,48 @@ static GtkWidget *gui_xml_box_new(xmlNode *cur, char dir)
     return gtk_vbox_new(arg1, arg2);
 }
 
-static GtkWidget *gui_dipsw(gui *g, xmlNode *cur)
+static GtkWidget *gui_xml_dipsw(gui *g, xmlNode *cur)
 {
     GtkWidget *wg0, *wg1, *wg2;
-    char tmp[8], *arg0, *desc;
+    char tmp[8], *arg0, *desc, rev = 0;
     int i=0, mask, len;
-    long set;
+    long set = 0;
+//    int size_x, size_y;
+    
+//    arg0 = (char *)xmlGetProp(cur, (unsigned char *)"size");
+//    if(arg0){
+//	sscanf(arg0, "%i, %i", &size_x, &size_y);
+	
+//    }
 
     arg0 = (char *)xmlGetProp(cur, (unsigned char *)"len");
     if(arg0) len = atoi(arg0);
     arg0 = (char *)xmlGetProp(cur, (unsigned char *)"value");
-    if(arg0) set = atoi(arg0);
+    if(arg0) set = strtol(arg0, NULL, 0);
     desc = (char *)xmlGetProp(cur, (unsigned char *)"name");
-
+    arg0 = (char *)xmlGetProp(cur, (unsigned char *)"reversed");
+    if(arg0) rev = !strcmp(arg0, "true");
+    
     wg0 = gtk_table_new(i,2, FALSE);
     memset(tmp, 0, 8);
+    if(len <= 0) return NULL;
     
-    for(mask =0x80, i = 0; i < len; i++,mask >>= 1){
-	sprintf(tmp, "%X", i + 1);
-	wg1 = gtk_label_new(tmp);
-	wg2 = gtk_image_new_from_stock(set & mask ? GUI_DIPSW_ON : GUI_DIPSW_OFF, g->icon_size);
-	gtk_table_attach(GTK_TABLE(wg0), wg1, i,i+1, 0,1, 0,0, 0,0);
-	gtk_table_attach(GTK_TABLE(wg0), wg2, i,i+1, 1,2, 0,0, 0,0);
-    }
+    if(rev)
+	for(mask = 1 << (len-1), i = 0; i < len; i++,mask >>= 1){
+	    sprintf(tmp, "%X", len - i);
+	    wg1 = gtk_label_new(tmp);
+	    wg2 = gtk_image_new_from_stock(set & mask ? GUI_DIPSW_ON : GUI_DIPSW_OFF, g->icon_size);
+	    gtk_table_attach(GTK_TABLE(wg0), wg1, i,i+1, 0,1, 0,0, 0,0);
+	    gtk_table_attach(GTK_TABLE(wg0), wg2, i,i+1, 1,2, 0,0, 0,0);
+	}
+    else
+	for(mask = 1, i = 0; i < len; i++,mask <<= 1){
+	    sprintf(tmp, "%X", i + 1);
+	    wg1 = gtk_label_new(tmp);
+	    wg2 = gtk_image_new_from_stock(set & mask ? GUI_DIPSW_ON : GUI_DIPSW_OFF, g->icon_size);
+	    gtk_table_attach(GTK_TABLE(wg0), wg1, i,i+1, 0,1, 0,0, 0,0);
+	    gtk_table_attach(GTK_TABLE(wg0), wg2, i,i+1, 1,2, 0,0, 0,0);
+	}
 
     if(desc){
 	wg1 = wg0;
@@ -168,7 +187,7 @@ static GtkWidget *gui_dipsw(gui *g, xmlNode *cur)
     return wg2;
 }
 
-static GtkWidget *gui_jumper(gui *g, xmlNode *cur)
+static GtkWidget *gui_xml_jumper(gui *g, xmlNode *cur)
 {
     GtkWidget *wg0, *wg1;
     char state, *idx_up, *idx_dn;
@@ -193,10 +212,174 @@ static GtkWidget *gui_jumper(gui *g, xmlNode *cur)
     return wg0;
 }
 
+static GtkWidget *gui_xml_table(gui *g, xmlNode *cur)
+{
+    int x=0, y=0;
+    char *tmp=NULL, eq=0;
+    
+    tmp = (char *)xmlGetProp(cur, (unsigned char *)"dim");
+    if(!tmp) return NULL;
+    sscanf(tmp, "%i, %i", &x, &y);
+
+    tmp = (char *)xmlGetProp(cur, (unsigned char *)"equal");
+    if(tmp) eq = !strcmp(tmp, "true");
+
+    if(!x || !y) return NULL;
+    return gtk_table_new(x, y, eq);
+}
+
+void gui_xml_signal_register(gui *g, GtkWidget *wg, char *id, char *signal, char *wg_name)
+{
+
+}
+
+static GtkWidget *gui_xml_chbutton(gui *g, xmlNode *cur)
+{
+    GtkWidget *tmp;
+    GtkWidget *tmp_2, * tmp_3, *tmp_4, *tmp_1;
+    GtkStyle *style;    
+    long fs;
+    char rl = 0, *arg, *id;
+
+    if(!(id = (char *)xmlGetProp(cur, (unsigned char *)"id"))){
+	return NULL;
+    };
+    arg = (char *)xmlGetProp(cur, (unsigned char *)"place");
+    if(arg) rl = !strcmp(arg, "right");
+    
+    tmp = gtk_alignment_new( rl ? 1:0, 0.5,0,0);
+    tmp_2 = gtk_hbox_new(FALSE,0);
+    gtk_container_add(GTK_CONTAINER(tmp), tmp_2);
+    tmp_3 = gtk_check_button_new();
+    gui_xml_signal_register(g, tmp_3, id, "toggled", "check_button");
+    tmp_4 = gtk_label_new((char *)xmlGetProp(cur, (unsigned char *)"label"));
+    style = gtk_widget_get_style(tmp_4);
+    fs = pango_font_description_get_size(style->font_desc);
+    pango_font_description_set_size(style->font_desc, fs / 1.3);
+    gtk_widget_modify_font(tmp_4,style->font_desc);
+    pango_font_description_set_size(style->font_desc, fs );
+
+    arg = (char *)xmlGetProp(cur, (unsigned char *)"sensitive");
+    if(arg){ 
+	if(!strcmp(arg, "false"))
+	    gtk_widget_set_sensitive(GTK_WIDGET(tmp_3), 0);
+    }
+    if(rl){
+	tmp_1 = tmp_4;
+	tmp_4 = tmp_3;
+	tmp_3 = tmp_1;
+    }
+    gtk_box_pack_start(GTK_BOX(tmp_2), tmp_3, FALSE,FALSE, 0); 
+    gtk_box_pack_start(GTK_BOX(tmp_2), tmp_4, FALSE,FALSE, 0); 
+
+    return tmp; 
+}
+
+static GtkWidget *gui_xml_button(gui *g, xmlNode *cur)
+{
+    GtkWidget *tmp;
+    char *arg, *id;
+        
+    if(!(id = (char *)xmlGetProp(cur, (unsigned char *)"id"))){
+	return NULL;
+    };
+    tmp = gtk_button_new_with_label((char *)xmlGetProp(cur, (unsigned char *)"label"));
+    arg = (char *)xmlGetProp(cur, (unsigned char *)"sensitive");
+    if(arg){ 
+	if(!strcmp(arg, "false"))
+	    gtk_widget_set_sensitive(GTK_WIDGET(tmp), 0);
+    }
+    gui_xml_signal_register(g, tmp, id, "clicked", "button");
+    return tmp;
+}
+
+static GtkWidget *gui_xml_spinbutton(gui *g, xmlNode *cur)
+{
+    GtkWidget *tmp1;
+    GtkAdjustment *adj;
+    char *arg, *id;
+    int max=0;    
+    
+    if(!(id = (char *)xmlGetProp(cur, (unsigned char *)"id"))){
+	return NULL;
+    };
+
+    arg = (char *)xmlGetProp(cur, (unsigned char *)"max");
+    if(arg) max = strtol(arg, NULL, 0);
+    adj = GTK_ADJUSTMENT(gtk_adjustment_new(0, 0.0, max, 1, 16, 16));    
+    tmp1 = gtk_spin_button_new(adj, 1, 0);
+    arg = (char *)xmlGetProp(cur, (unsigned char *)"size");
+    if(arg){ 
+	int xx=0, yy=0;
+	sscanf(arg, "%i, %i", &xx, &yy);
+	gtk_widget_set_usize(GTK_WIDGET(tmp1), xx, yy);
+    }
+
+    arg = (char *)xmlGetProp(cur, (unsigned char *)"sensitive");
+    if(arg){ 
+	if(!strcmp(arg, "false"))
+	    gtk_widget_set_sensitive(GTK_WIDGET(tmp1), 0);
+    }
+    gui_xml_signal_register(g, tmp1, id, "value_changed", "spin_button");
+    return tmp1;
+}
+
+static GtkWidget *gui_xml_entry(gui *g, xmlNode *cur)
+{
+    GtkWidget *tmp;
+    char *arg, *id;
+    
+    if(!(id = (char *)xmlGetProp(cur, (unsigned char *)"id"))){
+	return NULL;
+    };
+
+    tmp = gtk_entry_new();
+    arg = (char *)xmlGetProp(cur, (unsigned char *)"sensitive");
+    if(arg){ 
+	if(!strcmp(arg, "false"))
+	    gtk_widget_set_sensitive(GTK_WIDGET(tmp), 0);
+    }
+    
+    arg = (char *)xmlGetProp(cur, (unsigned char *)"editable");
+    if(arg){ 
+	if(!strcmp(arg, "false"))
+	    gtk_entry_set_editable(GTK_ENTRY(tmp), 0);
+    }
+    
+    arg = (char *)xmlGetProp(cur, (unsigned char *)"size");
+    if(arg){ 
+	int xx;
+	sscanf(arg, "%i", &xx);
+	gtk_entry_set_width_chars(GTK_ENTRY(tmp), xx);
+    }
+
+    gui_xml_signal_register(g, tmp, id, "changed", "entry");    
+    return tmp;
+}
+
+static GtkWidget *gui_xml_image(gui *g, xmlNode *cur)
+{
+    return gtk_image_new_from_file((char *)xmlGetProp(cur, (unsigned char *)"src"));
+}
+
+static GtkWidget *gui_xml_label(gui *g, xmlNode *cur)
+{
+    GtkWidget *tmp;
+    char *arg;
+    
+    tmp = gtk_label_new((char *)xmlGetProp(cur, (unsigned char *)"text"));
+    arg = (char *)xmlGetProp(cur, (unsigned char *)"align");
+    if(arg){ 
+	int xx, yy;
+	sscanf(arg, "%i, %i", &xx, &yy);
+	gtk_misc_set_alignment(GTK_MISC(tmp), (float)xx / 100, (float)yy / 100);
+    }
+    return tmp;
+}
+
 static void gui_xml_parse_element(gui *g, GtkWidget *wg, xmlDocPtr doc, xmlNode *cur, const char *parm)
 {
     char x = 0;
-    GtkWidget *tmp;
     char *arg0;
 
     if(parm == NULL) 
@@ -205,42 +388,46 @@ static void gui_xml_parse_element(gui *g, GtkWidget *wg, xmlDocPtr doc, xmlNode 
 	if(*parm == 0) x = 1;
     }
     for(; cur != NULL; cur = cur->next){
-//printf("|-->%s\n", cur->name);
-	if(!strcmp((char*)cur->name,"frame")){
-	    tmp = gui_xml_frame_new(cur);
-	    gui_xml_container_add(g, cur, doc, wg, tmp, 1, parm);
-	}
-	if(!strcmp((char*)cur->name,"vbox")){
-	    tmp = gui_xml_box_new(cur, 0);
-	    gui_xml_container_add(g, cur, doc, wg, tmp, 1, parm);
-	}
-	if(!strcmp((char*)cur->name,"hbox")){
-	    tmp = gui_xml_box_new(cur, 1);
-	    gui_xml_container_add(g, cur, doc, wg, tmp, 1, parm);
-	}
+	/* tagi kluczowe */
 	if(!strcmp((char*)cur->name,"if")){
 	    arg0 = (char *)xmlGetProp(cur, (unsigned char *)"chip");
-	    if(!strcmp(arg0, "none") && x) gui_xml_parse_element(g, wg, doc, cur->xmlChildrenNode, parm );
-	    if(!strcmp(arg0, parm) && x) gui_xml_parse_element(g, wg, doc, cur->xmlChildrenNode, NULL);
-	}
-	if(!strcmp((char*)cur->name,"description")){
+	    if(!strcmp(arg0, "none") && x) 
+	      gui_xml_parse_element(g, wg, doc, cur->xmlChildrenNode, parm );
+	    else
+	      if(strstr(arg0, parm) && x) gui_xml_parse_element(g, wg, doc, cur->xmlChildrenNode, NULL);
+	} 
+	else if(!strcmp((char*)cur->name,"description")){
 	    arg0 = (char*)xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
 	    gtk_label_set_text(GTK_LABEL(g->chip_desc), arg0);
-	}
-	if(!strcmp((char*)cur->name,"image")){
-	    arg0 = (char *)xmlGetProp(cur, (unsigned char *)"src");
-	    tmp = gtk_image_new_from_file(arg0);
-	    gui_xml_container_add(g, cur, doc, wg, tmp, 0, NULL);
-	}
-	if(!strcmp((char*)cur->name,"jumper")){
-	    tmp = gui_jumper(g, cur);
-	    gui_xml_container_add(g, cur, doc, wg, tmp, 0, NULL);
-	}
-	if(!strcmp((char*)cur->name, "dipswitch")){
-	    tmp = gui_dipsw(g, cur);
-	    gui_xml_container_add(g, cur, doc, wg, tmp, 0, NULL);
-	}
-	
+	} 
+	/**** elementy GUI ****/
+	/* kontenery */
+	else if(!strcmp((char*)cur->name,"frame"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_frame_new(cur), 1, parm);
+	else if(!strcmp((char*)cur->name,"vbox"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_box_new(cur, 0), 1, parm);
+	else if(!strcmp((char*)cur->name,"hbox"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_box_new(cur, 1), 1, parm);
+	else if(!strcmp((char*)cur->name, "table"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_table(g, cur), 1, parm);
+	/* kontrolki */
+	else if(!strcmp((char*)cur->name,"jumper"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_jumper(g, cur), 0, NULL);
+	else if(!strcmp((char*)cur->name, "dipswitch"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_dipsw(g, cur), 0, NULL);
+	else if(!strcmp((char*)cur->name,"image"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_image(g, cur), 0, NULL);
+	else if(!strcmp((char*)cur->name,"label"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_label(g, cur), 0, NULL);
+	else if(!strcmp((char*)cur->name, "button"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_button(g, cur), 0, NULL);
+	else if(!strcmp((char*)cur->name, "chbutton"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_chbutton(g, cur), 0, NULL);
+	else if(!strcmp((char*)cur->name, "spinbutton"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_spinbutton(g, cur), 0, NULL);
+	else if(!strcmp((char*)cur->name, "entry"))
+	    gui_xml_container_add(g, cur, doc, wg, gui_xml_entry(g, cur), 0, NULL);
+/* w przyszlosci dodac pozostale elementy w razie potrzeby */	
 
     }    
 }
@@ -249,7 +436,7 @@ static void gui_xml_parse_element(gui *g, GtkWidget *wg, xmlDocPtr doc, xmlNode 
 static void gui_xml_parser(gui *g, xmlDocPtr doc, const char *parm)
 {
     xmlNode *cur;
-    GtkWidget *tmp;
+    GtkWidget *tmp, *lab;
 
     if(!(cur = xmlDocGetRootElement(doc))) return;
     
@@ -261,7 +448,13 @@ static void gui_xml_parser(gui *g, xmlDocPtr doc, const char *parm)
 	    gtk_table_attach_defaults(GTK_TABLE(g->main_table), tmp, 1,2, 0, 2);
 	    gtk_widget_show_all(GTK_WIDGET(g->main_table));
 	}
-//	if(!strcmp((char*)cur->name,"notebook")) gui_xml_parse_element(g, GTK_WIDGET(g->notebook), doc, cur->xmlChildrenNode, parm);
+	if(!strcmp((char*)cur->name,"notebook")){
+	    lab = gtk_label_new((char *)xmlGetProp(cur, (unsigned char *)"name"));
+	    tmp = gtk_vbox_new(FALSE, 0);
+	     gui_xml_parse_element(g, tmp, doc, cur->xmlChildrenNode, parm);
+	    gtk_notebook_append_page(GTK_NOTEBOOK(g->notebook), tmp, lab);
+	    gtk_widget_show_all(GTK_WIDGET(g->notebook));
+	}
     }    
 }
 
@@ -316,4 +509,7 @@ void gui_drv_field_destroy(geepro *gep)
     GUI(gep->gui)->drv_vbox = NULL;
 }
 */
+
+
+
 
