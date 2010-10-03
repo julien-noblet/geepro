fedab3d78ca35a7bbf8b06ea9259242d08e3d340
krzkomar <krzkomar@2b195e0a-49af-4348-9a60-1f55b68a0e1c>
2010-10-03 09:28:02 +0000

    support for MCS-48 adapter (tested with 8049 chips)

--- trunk/geepro/plugins/mcs48.c
+++ trunk/geepro/plugins/mcs48.c
@@ -1,4 +1,4 @@
-/* $Revision: 1.3 $ */
+/* $Revision: 1.4 $ */
 /* geepro - Willem eprom programmer for linux
  * Copyright (C) 2006 Krzysztof Komarnicki
  * Email: krzkomar@wp.pl
@@ -23,122 +23,218 @@
 
 MODULE_IMPLEMENTATION
 
-/* ROM */
-#define SIZE_8048AH	1024
-#define SIZE_8049AH	2048
-#define SIZE_8050AH	4096
-/* EPROM */
-#define SIZE_8748H	1024
-#define SIZE_8749H	2048
-#define SIZE_8741	1024
-#define SIZE_8742	2048
+/* MEM SIZE */
+#define SIZE_8041	1024
+#define SIZE_8042	2048
+#define SIZE_8048	1024
+#define SIZE_8049	2048
+#define SIZE_8050	4096
 
-void read_8048(int size)
+/* MASK for T0 */
+#define MCS48_READ_MODE		0x10
+#define MCS48_WRITE_MODE	0x10
+
+void set_address_mcs48(int addr, char mode)
 {
-    unsigned short int addr;    
+    hw_delay(100);
+    set_address( ((addr >> 8) & 0x0f) | mode);
+    oe(0, 1500);  // ustawienie RESET = 0
+    set_data(addr & 0xff);
+    oe(1, 1500);  // zapamietanie dolnej polowki adresu
+}
 
-    start_action(0,0);
-    hw_sw_vpp(1);    
-    hw_delay(100000);
-    progress_loop(addr, size, "Odczyt"){
-	oe(0,3000);
-	set_address((addr >> 8) & 0x0f);
-	set_data(addr);
-	oe(1,1500);
-//	set_address(((addr >> 8) & 0x0f) | 0x10);
-	hw_delay(500);
-	copy_data_to_buffer(addr);
+void read_mcs48(int size)
+{
+    int addr;
+
+    TEST_CONNECTION( VOID )        
+    ce(0, 1);
+    oe(1, 1);
+    hw_sw_vcc(1);
+    hw_delay(3000);    
+    oe(0, 3000);  // RESET = 0, VPP = VCC
+    hw_sw_vpp(1); // VPP = Uvpp
+    oe(1, 3000);  // RESET = 1, VPP = VCC    
+    progress_loop(addr, size, "Reading"){
+	set_address_mcs48( addr, MCS48_READ_MODE );
+	copy_data_to_buffer( addr );
     }
+    set_address(0);
+    set_data(0);
     finish_action();
 }
 
-void verify_8048(int size)
+void test_blank_mcs48(int size)
 {
-    unsigned short int addr;    
-    char ok_status;
-
-    start_action(0,0);
-    hw_sw_vpp(1);    
-    hw_delay(100000);
-
-    ok_status = 1;
-    progress_loop(addr, size, "Weryfikacja"){
-	oe(0,3000);
-	set_address((addr >> 8) & 0x0f);
-	set_data(addr);
-	oe(1,3000);
-	cmp_data_and_buffer_ploop(addr, ok_status);
+    int addr;
+    char data, text[256];
+    TEST_CONNECTION( VOID )        
+    ce(0, 1);
+    oe(1, 1);
+    hw_sw_vcc(1);
+    hw_delay(3000);    
+    oe(0, 3000);  // RESET = 0, VPP = VCC
+    hw_sw_vpp(1); // VPP = Uvpp
+    oe(1, 3000);  // RESET = 1, VPP = VCC    
+    progress_loop(addr, size, "Test blank"){
+	set_address_mcs48( addr, MCS48_READ_MODE );
+	data = hw_get_data();
+	if( data != 0xff ){
+	    progressbar_free();
+	    break;
+	}
     }
+    set_address(0);
+    set_data(0);
     finish_action();
-    show_message(ok_status,"Weryfikacja", "Niezgodnosc danych","Dane zgodne z buforem");
+    if(data != 0xff){
+	sprintf(text, "[IF][TEXT] Memory is not clear\nByte 0x%x at address 0x%x [/TEXT][BR]OK", data & 0xff, addr & 0xffff);
+	show_message(0, text, NULL, NULL);    
+    }
 }
 
+void verify_mcs48(int size)
+{
+    int addr;
+    unsigned char rdata, wdata;
+    char text[256];
+    TEST_CONNECTION( VOID )        
+    ce(0, 1);
+    oe(1, 1);
+    hw_sw_vcc(1);
+    hw_delay(3000);    
+    oe(0, 3000);  // RESET = 0, VPP = VCC
+    hw_sw_vpp(1); // VPP = Uvpp
+    oe(1, 3000);  // RESET = 1, VPP = VCC    
+    progress_loop(addr, size, "Verifycation"){
+	set_address_mcs48( addr, MCS48_READ_MODE );
+	rdata = hw_get_data();
+	wdata = get_buffer( addr );
+	if( rdata != wdata ){
+	    progressbar_free();
+	    break;
+	}
+    }
+    set_address(0);
+    set_data(0);
+    finish_action();
+    if(rdata != wdata){
+	sprintf(text, 
+	    "[IF][TEXT] Buffer and chip inconsistent\n"
+	    "0x%x(chip) != 0x%x(buffer) at address 0x%x [/TEXT][BR]OK", 
+	    rdata & 0xff, wdata & 0xff, addr & 0xffff
+	);
+	show_message(0, text, NULL, NULL);    
+    }
+}
+
+void prog_pulse_mcs48()
+{
+    ce(0, 50000);
+    ce(1, 100);
+}
+
+void prog_mcs48(int size)
+{
+    unsigned int addr;
+    unsigned char rdata, wdata;
+    char text[256];
+    TEST_CONNECTION( VOID )        
+    ce(0, 1);
+    oe(1, 1);
+    hw_sw_vcc(1);
+    hw_delay(3000);    
+    oe(0, 3000);  // RESET = 0, VPP = VCC
+    hw_sw_vpp(1); // VPP = Uvpp
+    oe(1, 3000);  // RESET = 1, VPP = VCC    
+    progress_loop(addr, size, "Programm"){
+	set_address_mcs48( addr, MCS48_WRITE_MODE );
+	wdata = get_buffer( addr );
+	set_data( wdata );
+	prog_pulse_mcs48();	
+	set_address_mcs48( addr, MCS48_READ_MODE );
+	rdata = hw_get_data();
+	if( rdata != wdata ){
+	    progressbar_free();
+	    break;
+	}
+    }
+    set_address(0);
+    set_data(0);
+    finish_action();
+    if(rdata != wdata){
+	sprintf(text, 
+	    "[IF][TEXT] Program error\n"
+	    "0x%x(chip) != 0x%x(buffer) at address 0x%x [/TEXT][BR]OK", 
+	    rdata & 0xff, wdata & 0xff, addr & 0xffff
+	);
+	show_message(0, text, NULL, NULL);    
+    }
+}
+
+/********************************************************
+  ROM
+*/
+REGISTER_FUNCTION( read, 8048, mcs48, SIZE_8048 );
+REGISTER_FUNCTION( verify, 8048, mcs48, SIZE_8048 );
+REGISTER_FUNCTION( read, 8049, mcs48, SIZE_8049 );
+REGISTER_FUNCTION( verify, 8049, mcs48, SIZE_8049 );
+REGISTER_FUNCTION( read, 8050, mcs48, SIZE_8050 );
+REGISTER_FUNCTION( verify, 8050, mcs48, SIZE_8050 );
+/********************************************************
+ EPROM
+*/
+//REGISTER_FUNCTION( read, 8041, mcs48, SIZE_8041 );
+//REGISTER_FUNCTION( verify, 8041, mcs48, SIZE_8041 );
+//REGISTER_FUNCTION( prog, 8741, mcs48, SIZE_8041 );
+//REGISTER_FUNCTION( test_blank, 8041, mcs48, SIZE_8041 );
+//REGISTER_FUNCTION( read, 8042, mcs48, SIZE_8042 );
+//REGISTER_FUNCTION( verify, 8042, mcs48, SIZE_8042 );
+//REGISTER_FUNCTION( prog, 8742, mcs48, SIZE_8042 );
+//REGISTER_FUNCTION( test_blank, 8042, mcs48, SIZE_8042 );
+REGISTER_FUNCTION( prog, 8748, mcs48, SIZE_8048 );
+REGISTER_FUNCTION( test_blank, 8048, mcs48, SIZE_8048 );
+REGISTER_FUNCTION( prog, 8749, mcs48, SIZE_8049 );
+REGISTER_FUNCTION( test_blank, 8049, mcs48, SIZE_8049 );
+
 /*************************************************************************/
-REG_FUNC_BEGIN(read_8048AH) 
-    read_8048(SIZE_8048AH); 
-REG_FUNC_END
-
-REG_FUNC_BEGIN(verify_8048AH) 
-    verify_8048(SIZE_8048AH); 
-REG_FUNC_END
-
-REG_FUNC_BEGIN(read_8049AH) 
-    read_8048(SIZE_8049AH); 
-REG_FUNC_END
-
-REG_FUNC_BEGIN(verify_8049AH) 
-    verify_8048(SIZE_8049AH); 
-REG_FUNC_END
-
-REG_FUNC_BEGIN(read_8050AH) 
-    read_8048(SIZE_8050AH); 
-REG_FUNC_END
-
-REG_FUNC_BEGIN(verify_8050AH) 
-    verify_8048(SIZE_8050AH); 
-REG_FUNC_END
-
-REG_FUNC_BEGIN(read_8748H) 
-    read_8048(SIZE_8748H); 
-REG_FUNC_END
-
-//REG_FUNC_BEGIN(verify_8748AH() 
-//    verify_8748(SIZE_8748AH); 
-//REG_FUNC_END
-
-REG_FUNC_BEGIN(read_8749H) 
-    read_8048(SIZE_8749H); 
-REG_FUNC_END
-
-//REG_FUNC_BEGIN(verify_8749AH) 
-//    verify_8748(SIZE_8749AH); 
-//REG_FUNC_END
-
-/*************************************************************************/
-
 REGISTER_MODULE_BEGIN( MCS-48 )
-
-    register_chip_begin("/uk/MCS-48", "8048AH", "MCS-48", SIZE_8048AH);
-	add_action(MODULE_READ_ACTION, read_8048AH);
-	add_action(MODULE_VERIFY_ACTION, verify_8048AH);
+// ROM Version
+    register_chip_begin("/uk/MCS-48/ROM", "i8048", "MCS-48", SIZE_8048);
+	add_action(MODULE_READ_ACTION, read_8048);
+	add_action(MODULE_VERIFY_ACTION, verify_8048);
     register_chip_end;
-
-    register_chip_begin("/uk/MCS-48", "8049AH", "MCS-48", SIZE_8049AH);
-	add_action(MODULE_READ_ACTION, read_8049AH);
-	add_action(MODULE_VERIFY_ACTION, verify_8049AH);
+    register_chip_begin("/uk/MCS-48/ROM", "i8049", "MCS-48", SIZE_8049);
+	add_action(MODULE_READ_ACTION, read_8049);
+	add_action(MODULE_VERIFY_ACTION, verify_8049);
     register_chip_end;
-
-    register_chip_begin("/uk/MCS-48", "8050AH", "MCS-48", SIZE_8050AH);
-	add_action(MODULE_READ_ACTION, read_8050AH);
-	add_action(MODULE_VERIFY_ACTION, verify_8050AH);
+    register_chip_begin("/uk/MCS-48/ROM", "i8050", "MCS-48", SIZE_8050);
+	add_action(MODULE_READ_ACTION, read_8050);
+	add_action(MODULE_VERIFY_ACTION, verify_8050);
     register_chip_end;
-
-    register_chip_begin("/uk/MCS-48", "8748AH", "MCS-48", SIZE_8748H);
-	add_action(MODULE_READ_ACTION, read_8748H);
+// EPROM/OTP Version
+//    register_chip_begin("/uk/MCS-48/EPROM", "i8741", "MCS-48", SIZE_8041);
+//	add_action(MODULE_READ_ACTION, read_8041);
+//	add_action(MODULE_VERIFY_ACTION, verify_8041);
+//	add_action(MODULE_PROG_ACTION, prog_8741);
+//	add_action(MODULE_TEST_BLANK_ACTION, test_blank_8041);
+//    register_chip_end;
+//    register_chip_begin("/uk/MCS-48/EPROM", "i8742", "MCS-48", SIZE_8042);
+//	add_action(MODULE_READ_ACTION, read_8042);
+//	add_action(MODULE_VERIFY_ACTION, verify_8042);
+//	add_action(MODULE_PROG_ACTION, prog_8742);
+//	add_action(MODULE_TEST_BLANK_ACTION, test_blank_8042);
+//    register_chip_end;
+    register_chip_begin("/uk/MCS-48/EPROM", "i8748", "MCS-48", SIZE_8048);
+	add_action(MODULE_READ_ACTION, read_8048);
+	add_action(MODULE_VERIFY_ACTION, verify_8048);
+	add_action(MODULE_PROG_ACTION, prog_8748);
+	add_action(MODULE_TEST_BLANK_ACTION, test_blank_8048);
     register_chip_end;
-
-    register_chip_begin("/uk/MCS-48", "8749AH", "MCS-48", SIZE_8749H);
-	add_action(MODULE_READ_ACTION, read_8749H);
+    register_chip_begin("/uk/MCS-48/EPROM", "i8749", "MCS-48", SIZE_8049);
+	add_action(MODULE_READ_ACTION, read_8049);
+	add_action(MODULE_VERIFY_ACTION, verify_8049);
+	add_action(MODULE_PROG_ACTION, prog_8749);
+	add_action(MODULE_TEST_BLANK_ACTION, test_blank_8049);
     register_chip_end;
-
 REGISTER_MODULE_END
