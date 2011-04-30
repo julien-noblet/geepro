1edb6af6fee5805ed59dae0eb9efef2a7abb3167
krzkomar <krzkomar@2b195e0a-49af-4348-9a60-1f55b68a0e1c>
2011-04-30 10:29:13 +0000

    initial for HUB/LPC support

---
+++ geepro/chips/hub_lpc.c
@@ -0,0 +1,58 @@
+/* $Revision: 1.5 $ */
+/* geepro - Willem eprom programmer for linux
+ * Copyright (C) 2006 Krzysztof Komarnicki
+ * Email: krzkomar@wp.pl
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version. See the file COPYING. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "modules.h"
+
+MODULE_IMPLEMENTATION
+
+#define LF002_SIZE	KB_SIZE( 256 )
+#define LF003_SIZE	KB_SIZE( 384 )
+#define LF004_SIZE	KB_SIZE( 512 )
+#define LF008_SIZE	MB_SIZE( 1 )
+
+
+void read_LPC(unsigned int dev_size)
+{
+
+}
+
+/*********************************************************************************************/
+
+REGISTER_FUNCTION( read,  LF002, LPC, LF002_SIZE);
+
+REGISTER_MODULE_BEGIN(HUB_LPC)
+    register_chip_begin("/HUB LPC/SST49LFxxx", "SST49LF002", "HUB_LPC", LF002_SIZE);
+	add_action(MODULE_READ_ACTION, read_LF002);
+    register_chip_end;
+
+
+REGISTER_MODULE_END
+
+/*
+    lb = checkbox(
+	"[TITLE]Writing chip[/TITLE][TYPE:QS]"
+	"[CB:2:0: Are you sure ? (Tick if Yes)]"
+	"[CB:1:1: Verify after process]"
+    );
+
+    if( !lb ) return; // resignation by button
+    if( !(*lb & 2) ) return; // Not checked
+*/
