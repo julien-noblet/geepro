4f58f84a4737cd8fa39e9db9d262408838511055
krzkomar <krzkomar@2b195e0a-49af-4348-9a60-1f55b68a0e1c>
2013-08-17 16:47:18 +0000

    USB support from V-USB

---
+++ geepro/src/usb_reqs.h
@@ -0,0 +1,16 @@
+/* Name: requests.h
+   Project: Usb2Lpt converter for geepro
+   Author: Krzysztof Komarnicki
+   Creation Date: 17.08.2013
+   License: GNU GPL v2 (see License.txt), GNU GPL v3
+*/
+
+#ifndef __REQUESTS_H__
+#define __REQUESTS_H__
+
+#define USB_RQ_ECHO	0
+#define USB_RQ_OUTPUT	1
+#define USB_RQ_INPUT	2
+#define USB_RQ_ID	3
+#define USB_RQ_REV	4
+#endif 
